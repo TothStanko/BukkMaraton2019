f612d3afff9962988a6a48ad11c149a4a45090c7
Stanko <tothstanko@gmail.com>
2023-11-20 12:49:28 +0100

    Add project files.

---
+++ BukkMaraton2019/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace BukkMaraton2019
+{
+    internal class Program
+    {
+        static int SzamolasNoiVersenyzo(List<Versenytav> v)
+        {
+            int db = 0;
+
+            for (int i = 0; i < v.Count; i++)
+            {
+                if (v[i].Kategoria.Equals("n") && v[i].Rajtszam.Equals("R"))
+                {
+                    db++;
+                }
+
+            }
+
+            return db;
+        }
+
+
+        static void Main(string[] args)
+        {
+           List<Versenytav> maraton = new List<Versenytav>();
+
+            foreach (var item in File.ReadAllLines(@"../../../src/bukkm2019.txt"))
+            {
+                maraton.Add(new Versenytav(item));
+            }
+
+            foreach (var item in maraton)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("\n5.feladat");
+
+            var f5 = SzamolasNoiVersenyzo(maraton);
+            Console.WriteLine($"{f5}");
+
+
+        }
+    }
+}
---
+++ BukkMaraton2019/Versenytav.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BukkMaraton2019
+{
+    class Versenytav
+    {
+        public string Rajtszam { get; set; }
+        public string Kategoria { get; set; }
+        public string Nev { get; set; }
+        public string Egyesulet { get; set; }
+        public string Ido { get; set; }
+        public string Tav
+        {
+            get
+            {
+                switch (Rajtszam[0])
+                {
+                    case 'M': return "Mini";
+                    case 'R': return "Rövid";
+                    case 'K': return "Közép";
+                    case 'H': return "Hosszú";
+                    case 'E': return "Pedelec";
+                }
+                return "Hibás rajtszám";
+            }
+        }
+        public Versenytav(string rajtszam)
+        {
+            Rajtszam = rajtszam;
+            List<string> read = rajtszam.Split(";").ToList();
+
+            Rajtszam = read[0];
+            Kategoria = read[1];
+            Nev = read[2];
+            Egyesulet = read[3];  
+            Ido = read[4];
+
+
+        }
+
+        public override string ToString()
+        {
+            return $"Rajtszám: {Rajtszam} \nKategória: {Kategoria} \nNév: {Nev} \nEgyesület: {Egyesulet} \nIdő: {Ido}";
+        }
+
+    }
+
+}
+
+
